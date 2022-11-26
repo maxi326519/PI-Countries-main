cb2eb4a9f0b958fb0d8ed8a670dafe01aa110b88
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2022-11-26 00:14:39 -0300

    Add router

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -8,5 +8,40 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.get('/countries',(req, res)=>{
+    // Traer info de la base de datos
+    // Devolver un listado de las paises
+    try{
+
+    }catch(exception){
+        res.status(/* DEFINIR */).json({ error: exception.message });
+    }
+});
+
+router.get('/countries/:pais', (req, res)=>{
+    // Devolver detalles de los paises y actividades turisticas
+    try{
+
+    }catch(exception){
+        res.status(/* DEFINIR */).json({ error: exception.message });
+    }
+});
+
+router.get('/countries',(req, res)=>{
+    const { name } = req.query;
+    try{
+        // Devolver los paises que coincidan con el texto pasado por parametros (No exacto, que contenga)
+    }catch(exception){
+        res.status(/* DEFINIR */).json({ error: exception.message });
+    }
+});
+
+router.post('/activities', (req, res)=>{
+    try{
+
+    }catch(exception){
+        res.status(/* DEFINIR */).json({ error: exception.message });
+    }
+});
 
 module.exports = router;
